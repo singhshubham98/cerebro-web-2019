d053c2f75885807ba037e814fce2aecf32ce8927
Anshuman Verma <anshu.av97@gmail.com>
2019-02-14 20:35:07 +0530

    rm clg

--- src/components/App.js
+++ src/components/App.js
@@ -16,43 +16,34 @@ import requiresAuth from './requiresAuth';
 
 import '../sass/main.scss';
 
-console.log({
-	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
-})
-
 const store = getStore();
 
 class App extends Component {
-
 	componentDidMount() {
 		this.props.fetchUser();
 	}
 
 	render() {
-		return(
-				<BrowserRouter>
-					<div>
-						<Navbar />
-						<Social />
-						<div className="main">
-							<Switch>
-								<Route exact path="/" component={HomePage} />
-								<Route exact path="/about" component={About} />
-								<Route exact path="/dashboard" component={requiresAuth(MyEvents)} />
-								<Route exact path="/events/:id" component={Event} />
-							</Switch>
-						</div>
+		return (
+			<BrowserRouter>
+				<div>
+					<Navbar />
+					<Social />
+					<div className="main">
+						<Switch>
+							<Route exact path="/" component={HomePage} />
+							<Route exact path="/about" component={About} />
+							<Route exact path="/dashboard" component={requiresAuth(MyEvents)} />
+							<Route exact path="/events/:id" component={Event} />
+						</Switch>
 					</div>
-				</BrowserRouter>
-		)
+				</div>
+			</BrowserRouter>
+		);
 	}
 }
 
-
-
-export default connect(null, { fetchUser })(App);
+export default connect(
+	null,
+	{ fetchUser }
+)(App);
