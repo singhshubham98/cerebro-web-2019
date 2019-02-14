2228447f5dde3b64afd257d9a88f0caebb1224ce
Anshuman Verma <anshu.av97@gmail.com>
2019-02-14 19:19:10 +0530

    Build fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,81 +2,107 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import SignIn from './SignIn';
-import { signOut } from "../actions/authActions";
-
+import { signOut } from '../actions/authActions';
 
 class Navbar extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			drawerOut: false,
-		}
+			drawerOut: false
+		};
 	}
 
 	showDrawer = () => {
 		this.setState({
 			drawerOut: true
-		})
-		
+		});
+
 		try {
 			document.getElementById('user-profile-main').style.zIndex = -1;
 			document.getElementById('social-icons').style.zIndex = -1;
 		} catch {}
-		
-	}
+	};
 
 	hideDrawer = () => {
 		this.setState({
 			drawerOut: false
-		})
+		});
 		try {
 			document.getElementById('user-profile-main').style.zIndex = 0;
 			document.getElementById('social-icons').style.zIndex = 2000;
-		} catch {}	
-	}
+		} catch {}
+	};
 
 	render() {
+		const { auth, singnOut } = this.props;
 		return (
 			<nav className="navbar">
 				<div id="main-navbar" className="container">
-					<NavLink exact to="/">Home</NavLink>
+					<NavLink exact to="/">
+						Home
+					</NavLink>
 					<NavLink to="/about">About</NavLink>
 					<NavLink to="/team">Team</NavLink>
 					<NavLink to="/timeline">Timeline</NavLink>
 					<NavLink to="/events">Events</NavLink>
-          <NavLink to="/dashboard">My Events</NavLink>
-          {props.auth ? (
-            <button className='btn' onClick={props.signOut}>Sign Out</button>
-          ): (
-            <SignIn />
-          )}
+					<NavLink to="/dashboard">My Events</NavLink>
+					{auth ? (
+						<button className="btn" onClick={signOut}>
+							Sign Out
+						</button>
+					) : (
+						<SignIn />
+					)}
 				</div>
 				<div id="drawer-button" className="container">
-					<span id='drawer-button-span' onClick={this.showDrawer}><i class="fas fa-bars"></i></span>
+					<span id="drawer-button-span" onClick={this.showDrawer}>
+						<i class="fas fa-bars" />
+					</span>
 				</div>
 
-				<div id="black-background" className={this.state.drawerOut ? 'black-background-visible' : null}  onClick={this.hideDrawer}></div>
+				<div
+					id="black-background"
+					className={this.state.drawerOut ? 'black-background-visible' : null}
+					onClick={this.hideDrawer}
+				/>
 
 				<div id="drawer" className={this.state.drawerOut ? 'drawer-visible' : null}>
-					<NavLink onClick={this.hideDrawer} exact to="/">Home</NavLink>
-					<NavLink onClick={this.hideDrawer} to="/about">About</NavLink>
-					<NavLink onClick={this.hideDrawer} to="/team">Team</NavLink>
-					<NavLink onClick={this.hideDrawer} to="/timeline">Timeline</NavLink>
-					<NavLink onClick={this.hideDrawer} to="/events">Events</NavLink>
-					<NavLink onClick={this.hideDrawer} to="/dashboard">My Events</NavLink>
-          {props.auth ? (
-            <button className='btn' onClick={props.signOut}>Sign Out</button>
-          ): (
-            <SignIn />
-          )}
+					<NavLink onClick={this.hideDrawer} exact to="/">
+						Home
+					</NavLink>
+					<NavLink onClick={this.hideDrawer} to="/about">
+						About
+					</NavLink>
+					<NavLink onClick={this.hideDrawer} to="/team">
+						Team
+					</NavLink>
+					<NavLink onClick={this.hideDrawer} to="/timeline">
+						Timeline
+					</NavLink>
+					<NavLink onClick={this.hideDrawer} to="/events">
+						Events
+					</NavLink>
+					<NavLink onClick={this.hideDrawer} to="/dashboard">
+						My Events
+					</NavLink>
+					{auth ? (
+						<button className="btn" onClick={signOut}>
+							Sign Out
+						</button>
+					) : (
+						<SignIn />
+					)}
 				</div>
 			</nav>
 		);
-	}	
+	}
 }
 
 const mapStateToProps = ({ auth }) => {
-	return { auth }
-}
+	return { auth };
+};
 
-export default connect(mapStateToProps, { signOut })(Navbar);
+export default connect(
+	mapStateToProps,
+	{ signOut }
+)(Navbar);
